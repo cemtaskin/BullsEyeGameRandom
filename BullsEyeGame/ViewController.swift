9cce9ef5dae8f1dfcc90ce509f99148fe1f5ed40
CEM TASKIN <cemtaskin82@gmail.com>
2023-10-30 11:12:21 +0300

    Random value generator added

--- BullsEyeGame/ViewController.swift
+++ BullsEyeGame/ViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentValue : Int = 0
+    @IBOutlet weak var slider : UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        currentValue  = lroundf(slider.value)
+    }
+    
+    @IBAction func showAlert (){
+        let message = "The value of slider is :\(currentValue)"
+        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved (_ slider : UISlider){
+        currentValue  = lroundf(slider.value)
     }
 
 
